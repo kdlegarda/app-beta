80cebf2f3e756916cb8b144b08f96c2c1efd0d55
Kevin <kdlegarda.4711@unicesmag.edu.co>
2026-03-27 05:19:42 -1200

    avance ramas

--- src/singup.php
+++ src/singup.php
@@ -11,4 +11,43 @@
     //Execute query
     pg_query($sql);
     
-    ?>
+    //RAMA 1
+//email 
+$check_email = "SELECT email FROM users WHERE email = '$e_mail'";
+$res_email = pg_query($local_conn, $check_email);
+
+if (pg_num_rows($res_email) > 0) {
+    echo "Error: El correo electrónico '$e_mail' ya está registrado. Por favor, use uno diferente.\n";
+    exit();
+
+
+$res_local = pg_query($local_conn, $sql);
+}
+
+//RAMA 2
+
+//telefono 
+$check_phone = "SELECT mobile_phone FROM users WHERE mobile_phone = '$m_phone'";
+$res_phone = pg_query($local_conn, $check_phone);
+
+if (pg_num_rows($res_phone) > 0) {
+    echo "Error: El número de celular '$m_phone' ya está registrado en nuestro sistema."; 
+    exit();
+}
+
+//RAMA 3 
+
+if ($res_local) {
+    // --- PASO B: Si funcionó el anterior, guardar en la nube (Supabase) ---
+    $res_supa = pg_query($supa_conn, $sql);
+
+    if ($res_supa) {
+        echo "¡Listo! Guardado en ambos lados.";
+    } else {
+        echo "Error: Se guardó en local pero no en la nube.";
+    }
+} else {
+    echo "Error: No se pudo guardar ni en local.";
+}
+$enc_pass = password_hash($p_sword, PASSWORD_BCRYPT);
+ ?>
