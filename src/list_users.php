7d912583e9cff610c56e8d599ffe580dcdfdeeb9
Kevin <kdlegarda.4711@unicesmag.edu.co>
2026-04-10 04:59:13 -1200

    tablas nuevas

---
+++ src/list_users.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" align ="center">
+        <tr>
+            <th> Fullname </th>
+            <th>E-mail </th>
+            <th> Mobile Phone </th>
+            <th> Status </th>
+            <th> Photo </th>
+            <th> Options </th>
+        </tr>
+        <tr>
+            <td> Peter Loza </td>
+            <td> peter@gmail.com </td>
+            <td> 300123 </td>
+            <td> Active </td>
+            <td> <img src="profile_photos/user_default.png" width="50" alt="User photo"></td>
+            <td>
+                <a href="#">
+                    <img src="icons/edit.png" width="20" alt ="Edit user">
+                </a>
+                &nbsp;&nbsp;
+                <a href="#">
+                    <img src="icons/delete.png" width="20" alt ="Delete user">
+                </a>
+            </td>
+    </table>
+</body>
+</html> 
--- src/singup.php
+++ src/singup.php
@@ -1,14 +1,43 @@
 <?php
-    include('../config/database.php');
-    //get data
-    $f_name =$_POST['fname'];
-    $l_name  =$_POST['lname'];
-    $e_mail =$_POST['email'];
-    $m_phone=$_POST['mphone'];
-    $p_sswd =$_POST['passwd'];
-    //query to instert into SQL.
-    $sql="INSERT INTO users (nombre,apellido,email,telefono,contraseña) VALUES('$f_name','$l_name','$e_mail','$m_phone','$p_sswd')";
-    //Execute query
-    pg_query($sql);
-    
-    ?>
+include ('../config/database.php');
+//getdata
+$f_name = $_POST ['fname'];
+$l_name = $_POST ['lname'];
+$m_phone = $_POST ['mphone'];
+$e_mail= $_POST ['email'];
+$p_sswd= $_POST ['passwd'];
+$enc_pass = md5($p_sswd);
+// 🔐 FEATURE 4: ENCRIPTAR
+$enc_pass = password_hash($p_sswd, PASSWORD_DEFAULT);
+
+// ❗ VALIDAR CONTRASEÑAS
+if($p_sswd != $confirm){
+    echo "Las contraseñas no coinciden";
+    exit;
+}
+
+// 🔍 FEATURE 1: VALIDAR EMAIL
+$check_email = pg_query($local_conn, "SELECT * FROM users WHERE email='$e_mail'");
+if(pg_num_rows($check_email) > 0){
+    echo "El correo ya está registrado";
+    exit;
+}
+
+// 🔍 FEATURE 2: VALIDAR TELÉFONO
+$check_phone = pg_query($local_conn, "SELECT * FROM users WHERE telefono='$m_phone'");
+if(pg_num_rows($check_phone) > 0){
+    echo "El teléfono ya está registrado";
+    exit;
+}
+
+// 📥 INSERT
+$sql = "INSERT INTO users (nombre, apellido, telefono, email, contrasena)
+VALUES ('$f_name','$l_name','$m_phone','$e_mail','$enc_pass')";
+
+// 💾 FEATURE 3: GUARDAR EN AMBAS
+pg_query($local_conn, $sql);
+pg_query($supa_conn, $sql);
+
+echo "Registro exitoso 🚀";
+
+?>
